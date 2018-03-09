22590b06b6413bf962f857cb0d1170c50aa4df26
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-03-09 18:09:16 -0500

    Actualizacion del servidor y cliente. Transferencia de archivos

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -1,17 +1,31 @@
 import socket
 
-port = 'www', 'integralist', 'co.uk', 80
-
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 nombre_servidor = '127.0.0.1'
 puerto_servidor = 5005
+TAM_BUFFER = 1024
+print('Intentare conectarme a %s:%d'%(nombre_servidor, puerto_servidor))
 cliente.connect((nombre_servidor, puerto_servidor))
-
-mensaje = 'Este es un mensaje'
-cliente.sendto(mensaje.encode(),(nombre_servidor, puerto_servidor))
-
-respuesta = cliente.recv(4096)
-
-print (respuesta)
-
+print('conectado')
+mensaje = input('Ingrese el nombre del archivo a descargar: ')
+while mensaje:
+    print('Pedi %s'%mensaje)
+    cliente.sendto(mensaje.encode(),(nombre_servidor, puerto_servidor))
+    print('Mensaje enviado')
+    archivo_recibir = cliente.recv(TAM_BUFFER)
+    print ('Recibiendo archivo')
+    print(type(archivo_recibir))
+    print(archivo_recibir)
+    if not archivo_recibir == 'No existe':
+        f = open(mensaje, 'wb')
+        while archivo_recibir:
+            print('Recibiendo...')
+            f.write(archivo_recibir)
+            archivo_recibir = cliente.recv(TAM_BUFFER)
+        print('Termine de recibir')
+        f.close()
+    else:
+        mensaje = input('Por favor vuelva a ingresar el nombre del archivo')
+    mensaje = ''
+print('Terminada comunicacion')
 cliente.close()
--- servidor_tcp.py
+++ servidor_tcp.py
@@ -1,16 +1,19 @@
 import socket
 import threading
+import os.path
 
+#Este servidor recibe conexion de un solo cliente al tiempo (no hay threading)
+#ip y puertos del servidor
 bind_ip = '127.0.0.1'
 bind_port = 5005
-
+TAM_BUFFER = 1024
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind((bind_ip, bind_port))
 servidor.listen(5)
 
 print ('Escuchando en (ip:puerto){}:{}'.format(bind_ip, bind_port))
 
-
+#funcion para usar threading.
 def manejador_conexion(socket_cliente, nombre_cliente, puerto_cliente):
     request = socket_cliente.recv(1024)
     print ('Recibi: {}'.format(request))
@@ -20,10 +23,27 @@ def manejador_conexion(socket_cliente, nombre_cliente, puerto_cliente):
     socket_cliente.close()
 
 while True:
+    print('aceptando conexion!')
     socket__conexion_servidor_cliente, direccion = servidor.accept()
+    nombre_cliente = direccion[0]
+    puerto_cliente = direccion[1]
     print ('Se acepto una conexion desde {}:{}'.format(direccion[0], direccion[1]))
-    client_handler = threading.Thread(
-        target=manejador_conexion,
-        args=(socket__conexion_servidor_cliente, direccion[0], direccion[1])  #con la coma!!
-    )
-    client_handler.start()
+    ##Esto inicia el threading en caso de que se espere mas de un cliente
+    #client_handler = threading.Thread(
+    #    target=manejador_conexion,
+    #    args=(socket__conexion_servidor_cliente, direccion[0], direccion[1])  #con la coma!!
+    #)
+    #client_handler.start()
+    peticion = socket__conexion_servidor_cliente.recv(TAM_BUFFER)
+    print ('Recibi: {}'.format(peticion))
+    if os.path.isfile(peticion):
+        f = open(peticion, 'rb')
+        archivo_enviar = f.read(TAM_BUFFER)
+        while archivo_enviar:
+            socket__conexion_servidor_cliente.send(archivo_enviar)
+            archivo_enviar = f.read(TAM_BUFFER)
+        f.close()
+        socket__conexion_servidor_cliente.shutdown(socket.SHUT_WR)
+    else:
+        socket__conexion_servidor_cliente.sendto('No existe'.encode(), (nombre_cliente, puerto_cliente))
+    socket__conexion_servidor_cliente.close()
